6412500ed1b27127dc56978d83b7e74cb8b0f695
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-11 16:55:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+const msg = ["first step: start", "second step:sec", "thisrd step: third"];
+
+function App() {
+  const step = 1;
+
+  return (
+    <div className="app">
+      <div className="num">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+      </div>
+      <div className="msg">
+        {" "}
+        Step {step} is : {msg[step - 1]}
+      </div>
+      <div className="btn">
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
