59189471b721deee303a2393ce763563756909ee
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-12 10:56:49 +0300

    filter()

--- src/pages/PackingList.jsx
+++ src/pages/PackingList.jsx
@@ -1,14 +1,53 @@
+/************/
+//Filtering arrays of items
 const people = [
-  "Creola Katherine Johnson: mathematician",
-  "Mario José Molina-Pasquel Henríquez: chemist",
-  "Mohammad Abdus Salam: physicist",
-  "Percy Lavon Julian: chemist",
-  "Subrahmanyan Chandrasekhar: astrophysicist",
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
 ];
 
-function PackingList() {
-  const list = people.map((person) => <li>{person}</li>);
+export default function DisplayList() {
+  const chemists = people.filter((person) => person.profession === "chemist");
+  const list = chemists.map((l) => <li>{l.name} </li>);
+
   return <ul>{list}</ul>;
 }
 
-export default PackingList;
+/************/
+//using map()
+// const people = [
+//   "Creola Katherine Johnson: mathematician",
+//   "Mario José Molina-Pasquel Henríquez: chemist",
+//   "Mohammad Abdus Salam: physicist",
+//   "Percy Lavon Julian: chemist",
+//   "Subrahmanyan Chandrasekhar: astrophysicist",
+// ];
+
+// export default function PackingList() {
+//   const list = people.map((person) => <li>{person}</li>);
+//   return <ul>{list}</ul>;
+// }
+//end of map()
+/************/
