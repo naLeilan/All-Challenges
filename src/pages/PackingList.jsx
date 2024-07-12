7ec56323d35e9a15bccf29be9abb67616876b9a2
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-12 17:06:07 +0300

    nesting two different map

--- src/pages/PackingList.jsx
+++ src/pages/PackingList.jsx
@@ -1,49 +1,70 @@
-/************/
-//Filtering arrays of items
-const people = [
-  {
-    id: 0,
-    name: "Creola Katherine Johnson",
-    profession: "mathematician",
-  },
-  {
-    id: 1,
-    name: "Mario José Molina-Pasquel Henríquez",
-    profession: "chemist",
-  },
-  {
-    id: 2,
-    name: "Mohammad Abdus Salam",
-    profession: "physicist",
-  },
-  {
-    id: 3,
-    name: "Percy Lavon Julian",
-    profession: "chemist",
-  },
-  {
-    id: 4,
-    name: "Subrahmanyan Chandrasekhar",
-    profession: "astrophysicist",
-  },
-];
+/***********nesting two different map */
+import { recipes } from "../../public/data";
 
-export default function DisplayList() {
-  const chemists = people.filter((person) => person.profession === "chemist");
-  const everyone = people.filter((person) => person.profession !== "chemist");
-  const list = chemists.map((l) => <li key={l.id}>{l.name} </li>);
-  const others = everyone.map((p) => <li key={p.id}>{p.name} </li>);
-
+export default function RecipeList() {
   return (
     <div>
-      {" "}
-      <h2>chemists</h2>
-      <ul>{list} </ul>
-      <h2>Everyone Else</h2>
-      <ul>{others} </ul>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
+
+/************/
+//Filtering arrays of items
+// const people = [
+//   {
+//     id: 0,
+//     name: "Creola Katherine Johnson",
+//     profession: "mathematician",
+//   },
+//   {
+//     id: 1,
+//     name: "Mario José Molina-Pasquel Henríquez",
+//     profession: "chemist",
+//   },
+//   {
+//     id: 2,
+//     name: "Mohammad Abdus Salam",
+//     profession: "physicist",
+//   },
+//   {
+//     id: 3,
+//     name: "Percy Lavon Julian",
+//     profession: "chemist",
+//   },
+//   {
+//     id: 4,
+//     name: "Subrahmanyan Chandrasekhar",
+//     profession: "astrophysicist",
+//   },
+// ];
+
+// export default function DisplayList() {
+//   const chemists = people.filter((person) => person.profession === "chemist");
+//   const everyone = people.filter((person) => person.profession !== "chemist");
+//   const list = chemists.map((l) => <li key={l.id}>{l.name} </li>);
+//   const others = everyone.map((p) => <li key={p.id}>{p.name} </li>);
+
+//   return (
+//     <div>
+//       {" "}
+//       <h2>chemists</h2>
+//       <ul>{list} </ul>
+//       <h2>Everyone Else</h2>
+//       <ul>{others} </ul>
+//     </div>
+//   );
+// }
 
 /************/
 //using map()
