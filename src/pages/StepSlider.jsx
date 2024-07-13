48015c1d74149130a415497456c25a2c3fa7a534
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-13 08:37:17 +0300

    aded logic

--- src/pages/StepSlider.jsx
+++ src/pages/StepSlider.jsx
@@ -5,19 +5,31 @@ const msg = ["first step: start", "second step:sec", "thisrd step: third"];
 function StepSlider() {
   const [step, setStep] = useState(1);
 
+  function handlePrev() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
         <p className={step >= 1 ? "active" : ""}>1</p>
-        <p className="nums">2</p>
-        <p className="nums">3</p>
+        <p className={step >= 2 ? "active" : ""}>2</p>
+        <p className={step >= 3 ? "active" : ""}>3</p>
       </div>
       <div className="steps-msg">
         Step {step} is : {msg[step - 1]}
       </div>
       <div className="steps-btn">
-        <button className="btn">Prev</button>
-        <button className="btn">Next</button>
+        <button className="btn" onClick={handlePrev}>
+          Prev
+        </button>
+        <button className="btn" onClick={handleNext}>
+          Next
+        </button>
       </div>
     </div>
   );
