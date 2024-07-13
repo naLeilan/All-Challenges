1245ec591c00fabf7d9f0dfc2ae2e5c2791a10ec
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-13 08:32:53 +0300

    aded stepslider css

--- src/pages/StepSlider.jsx
+++ src/pages/StepSlider.jsx
@@ -1,19 +1,21 @@
+import { useState } from "react";
+
 const msg = ["first step: start", "second step:sec", "thisrd step: third"];
 
 function StepSlider() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   return (
-    <div className="stepslider">
-      <div className="stepslider-num">
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
+    <div className="steps">
+      <div className="numbers">
+        <p className={step >= 1 ? "active" : ""}>1</p>
+        <p className="nums">2</p>
+        <p className="nums">3</p>
       </div>
-      <div className="stepslider-msg">
+      <div className="steps-msg">
         Step {step} is : {msg[step - 1]}
       </div>
-      <div className="stepslider-btn">
+      <div className="steps-btn">
         <button className="btn">Prev</button>
         <button className="btn">Next</button>
       </div>
