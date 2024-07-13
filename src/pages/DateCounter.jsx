c0b11b72aeb38a567e2bcaad2867ea754e65beee
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-13 09:31:59 +0300

    aded logic phase 1

--- src/pages/DateCounter.jsx
+++ src/pages/DateCounter.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
+
 function DateCounter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function handleStepPrev() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleStepNext() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountPrev() {
+    if (count > 1) setCount((c) => c - 1);
+  }
+
+  function handleCountNext() {
+    setCount((c) => c + 1);
+  }
+
   return (
     <div className="datecount">
       <h4>Date Counter</h4>
       <div className="step">
-        <button className="btn">-</button>
-        <p>Step:</p>
-        <button className="btn">+</button>
+        <button className="btn" onClick={handleStepPrev}>
+          -
+        </button>
+        <p>Step:{step} </p>
+        <button className="btn" onClick={handleStepNext}>
+          +
+        </button>
       </div>
       <div className="count">
-        <button className="btn">-</button>
-        <p>Count:</p>
-        <button className="btn">+</button>
+        <button className="btn" onClick={handleCountPrev}>
+          -
+        </button>
+        <p>Count:{count} </p>
+        <button className="btn" onClick={handleCountNext}>
+          +
+        </button>
       </div>
       <p>Today is ...</p>
     </div>
