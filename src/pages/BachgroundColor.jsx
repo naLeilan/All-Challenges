c0da2f892182a48f494e44ae7561463b780f24ee
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-08-01 14:27:43 +0300

    added change background

--- src/pages/BachgroundColor.jsx
+++ src/pages/BachgroundColor.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
+const COLORS = [
+  "pink",
+  "red",
+  "yellow",
+  "blue",
+  "green",
+  "grey",
+  "orange",
+  "purple",
+];
+
 function BachgroundColor() {
-  return <div>BachgroundColor</div>;
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+
+  const handleButton = (color) => () => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor,
+      }}
+    >
+      <h2> Change Background Color </h2>
+      <div>
+        {COLORS.map((color) => (
+          <button
+            type="button"
+            key={color}
+            onClick={handleButton(color)}
+            className={backgroundColor === color ? "circle selected" : "circle"}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default BachgroundColor;
